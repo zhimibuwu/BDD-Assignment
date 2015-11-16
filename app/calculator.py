fcb402380c8d4032d2574e26518d6954fb3a2c48
NitishMhalgi <nitishmhalgi92@gmail.com>
2015-11-15 20:22:29 -0500

    Adding skeleton code

--- app/calculator.py
+++ app/calculator.py
@@ -16,4 +16,25 @@ class Calculator(object):
         else:
             raise ValueError
 
+    def volume(self, l, h, b):
+        number_types = (int, long, float, complex)
+        if isinstance(l, number_types) and isinstance(h, number_types) and isinstance(b, number_types):
+            #TODO compute volume here
+            return 0
+        else:
+            raise ValueError
+
+
+    def factorial(self, n):
+        number_types = (int, long)
+ 
+        if isinstance(n, number_types):
+            if n < 0:
+                return None
+            else:
+                #TODO compute factorial here
+                return 0
+        else:
+            raise ValueError
+
     
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -18,7 +18,16 @@ def step_for_add(step, x, y):
 def step_for_mutiply(step, x, y):
     world.result = world.calc.multiply(int(x), int(y))
 
-	
+
+'''
+#TODO finish step for volume
+def step_for_volume(step, x, y, z):
+    
+#TODO finish step for factorial
+def step_for_factorial(step, x):
+'''
+
+
 @step(u'I should see "([^"]+)"')
 def result(step, expected_result):
     actual_result = world.result
